586cd98670dc55538cb9062e51a393bfe44b7fd9
TripleKay <triplekay.k3@gmail.com>
2022-06-14 14:38:34 +0630

    dashboard added

---
+++ resources/views/admin/brand/index.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="row">
+        <h1>hello brands</h1>
+    </div>
+@endsection
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.layouts.app')
+@section('content')
+    <div class="row">
+        <h1>hello dashboard</h1>
+    </div>
+@endsection
