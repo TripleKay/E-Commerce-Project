adb67a208b820dcf9d64b9238978db5a3b2ea910
TripleKay <triplekay.k3@gmail.com>
2022-08-13 23:41:04 +0630

    dashboard page with chart

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,37 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\ProductVariant;
 
 class DashboardController extends Controller
 {
     //index
     public function index(){
-        return view('admin.dashboard');
+        $orders = Order::select(DB::raw("COUNT(*) as count"))
+                            ->whereYear('created_at',date('Y'))
+                            ->groupBy(DB::raw('Month(created_at)'))
+                            ->pluck('count');
+        $totalSales = Order::select(DB::raw("SUM(grand_total) as totalSales"))
+                            ->whereYear('created_at',date('Y'))
+                            ->groupBy(DB::raw('Month(created_at)'))
+                            ->pluck('totalSales');
+        $months = Order::select(DB::raw("Month(created_at) as month"))
+                            ->whereYear('created_at',date('Y'))
+                            ->groupBy(DB::raw('Month(created_at)'))
+                            ->pluck('month');
+        $data = [0,0,0,0,0,0,0,0,0,0,0,0];
+        $salesByMonth = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+        foreach($months as $index=>$month){
+            $data[$month] = $orders[$index];
+            $salesByMonth[$month] = $totalSales[$index];
+        }
+
+        return view('admin.dashboard')->with(['data'=>$data,'salesByMonth'=>$salesByMonth]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,15 +1,23 @@
 @extends('admin.layouts.app')
 @section('content')
+    @php
+        $now = Carbon\Carbon::now();
+        $todaySales = App\Models\Order::where('order_date',$now->format('d F Y'))->sum('grand_total');
+        $monthlySales = App\Models\Order::where('order_month',$now->format('F'))->sum('grand_total');
+        $yearlySales = App\Models\Order::where('order_year',$now->format('Y'))->sum('grand_total');
+        $pendingOrders = App\Models\Order::where('status','pending')->count();
+
+    @endphp
     <div class="row pt-4 mb-3">
         <div class="col-3">
             <div class="card shadow-none">
                 <div class="card-body d-flex align-items-center">
                     <div class="p-3 rounded">
-                        <i class="fas fa-shopping-bag text-primary" style="font-size: 40px"></i>
+                        <i class="fas fa-calendar-day text-primary" style="font-size: 40px"></i>
                     </div>
                     <div class="ml-3">
-                        <h3 class="mb-0 font-weight-bolder">23</h3>
-                        <h5 class="mb-0">Total Orders</h5>
+                        <h6 class="mb-0 text-secondary">Today's Sale</h6>
+                        <h4 class="mb-0 font-weight-bolder">{{ number_format($todaySales) }} Ks</h4>
                     </div>
                 </div>
             </div>
@@ -21,8 +29,8 @@
                         <i class="fas fa-th text-primary" style="font-size: 40px"></i>
                     </div>
                     <div class="ml-3">
-                        <h3 class="mb-0 font-weight-bolder">23</h3>
-                        <h5 class="mb-0">Total Products</h5>
+                        <h6 class="mb-0 text-secondary">Monthly's Sale</h6>
+                        <h4 class="mb-0 font-weight-bolder">{{ number_format($monthlySales) }} Ks</h4>
                     </div>
                 </div>
             </div>
@@ -31,11 +39,11 @@
             <div class="card shadow-none">
                 <div class="card-body d-flex align-items-center">
                     <div class=" p-3 rounded">
-                        <i class="fas fa-users text-primary" style="font-size: 40px"></i>
+                        <i class="fas fa-calendar-alt text-primary" style="font-size: 40px"></i>
                     </div>
                     <div class="ml-3">
-                        <h3 class="mb-0 font-weight-bolder">23</h3>
-                        <h5 class="mb-0">Total Customers</h5>
+                        <h6 class="mb-0 text-secondary">Yearly Sale</h6>
+                        <h4 class="mb-0 font-weight-bolder">{{ number_format($yearlySales) }} Ks</h4>
                     </div>
                 </div>
             </div>
@@ -44,11 +52,12 @@
             <div class="card shadow-none">
                 <div class="card-body d-flex align-items-center">
                     <div class=" p-3 rounded">
-                        <i class="fas fa-map-marker-alt text-primary" style="font-size: 40px"></i>
+                        <i class="fas fa-shopping-bag text-primary" style="font-size: 40px"></i>
+
                     </div>
                     <div class="ml-3">
-                        <h3 class="mb-0 font-weight-bolder">23</h3>
-                        <h5 class="mb-0">Support Location</h5>
+                        <h6 class="mb-0 text-secondary">Pending Order</h6>
+                        <h4 class="mb-0 font-weight-bolder">{{ $pendingOrders }} Orders</h4>
                     </div>
                 </div>
             </div>
@@ -58,7 +67,7 @@
         <div class="col-6">
             <div class="card shadow-none rounded">
                 <div class="card-header">
-                    <h5 class="mb-0">Product By Category</h5>
+                    <h5 class="mb-0">Orders By Month ( {{ date('Y')}} )</h5>
                 </div>
                 <div class="card-body">
                     <canvas id="myChart"></canvas>
@@ -68,7 +77,7 @@
         <div class="col-6">
             <div class="card shadow-none rounded">
                 <div class="card-header">
-                    <h5 class="mb-0">Order By Months</h5>
+                    <h5 class="mb-0">Sales By Month ( {{ date('Y')}} )</h5>
                 </div>
                 <div class="card-body">
                     <canvas id="orderByMonth"></canvas>
@@ -86,16 +95,17 @@
                </div>
             </div>
             <div class="card-body">
+                @php
+                    $products = App\Models\ProductVariant::query()->with(['product' => function($query){
+                                    $query->select('product_id','name','publish_status');
+                                },'color','size'])->where('available_stock','<=','5')->get();
+                @endphp
                 <div class="table-responsive">
                     <table class="table table-hover" id="dataTable">
                         <thead class="bg-primary text-nowrap">
                           <tr>
                             <th>#</th>
-                            <th>Preview Image</th>
                             <th>Name</th>
-                            <th>Brand</th>
-                            <th>Category</th>
-
                             <th>Color</th>
                             <th>Size</th>
                             <th>Stocks</th>
@@ -104,45 +114,27 @@
                           </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>
-                                    <img src="http://127.0.0.1:8000/uploads/products/62b1bcc72863c_mobilePhone4.jpg" class="rounded shadow-sm" alt="" srcset="" style="width: 90px; height: 90px;">
-                                </td>
-                                <td>Iphone 11 (16:128)</td>
-                                <td>Apple</td>
-                                <td>Mobile Phone</td>
-
-                                <td>White</td>
-                                <td>---</td>
-                                <td>5</td>
-                                <td>Publish</td>
-                                <td class="text-wrap">
-                                    <a href="http://127.0.0.1:8000/admin/product/detail/1" class="btn btn-info btn-sm mb-2"><i class="fas fa-eye "></i></a>
-                                    <a href="http://127.0.0.1:8000/admin/product/edit/1" class="btn btn-success btn-sm mb-2"><i class="fas fa-edit "></i></a>
-                                    <a href="http://127.0.0.1:8000/admin/product/variant/create/1" class="btn btn-dark btn-sm mb-2"><i class="fas fa-plus-circle"></i></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>1</th>
-                                <td>
-                                    <img src="http://127.0.0.1:8000/uploads/products/62b1bcc72863c_mobilePhone4.jpg" class="rounded shadow-sm" alt="" srcset="" style="width: 90px; height: 90px;">
-                                </td>
-                                <td>Iphone 11 (16:128)</td>
-                                <td>Apple</td>
-                                <td>Mobile Phone</td>
-
-                                <td>White</td>
-                                <td>---</td>
-                                <td>5</td>
-                                <td>Publish</td>
-                                <td class="text-wrap">
-                                    <a href="http://127.0.0.1:8000/admin/product/detail/1" class="btn btn-info btn-sm mb-2"><i class="fas fa-eye "></i></a>
-                                    <a href="http://127.0.0.1:8000/admin/product/edit/1" class="btn btn-success btn-sm mb-2"><i class="fas fa-edit "></i></a>
-                                    <a href="http://127.0.0.1:8000/admin/product/variant/create/1" class="btn btn-dark btn-sm mb-2"><i class="fas fa-plus-circle"></i></a>
-                                </td>
-                            </tr>
-                            
+                            @foreach ($products as $item)
+                                <tr>
+                                    <th>{{ $item->product_variant_id }}</th>
+                                    <td>{{ $item->product->name }}</td>
+                                    <td>{{ $item->color_id != null ? $item->color->name : '---'  }}</td>
+                                    <td>{{ $item->size_id != null ? $item->size->name : '---'  }}</td>
+                                    <td>{{ $item->available_stock }}</td>
+                                    <td>
+                                        @if ($item->product->publish_status == 1)
+                                            <div class="badge badge-success">Publish</div>
+                                        @else
+                                            <div class="badge badge-danger">Unpublish</div>
+                                        @endif
+                                    </td>
+                                    <td class="text-wrap">
+                                        <a href="{{ route('admin#showProduct',$item->product_id) }}" class="btn btn-info btn-sm mb-2"><i class="fas fa-eye "></i></a>
+                                        <a href="{{ route('admin#editVariant',$item->product_variant_id) }}" class="btn btn-success btn-sm mb-2"><i class="fas fa-edit "></i></a>
+                                        <a href="{{ route('admin#createVariant',$item->product_id) }}" class="btn btn-dark btn-sm mb-2"><i class="fas fa-plus-circle"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                       </table>
                 </div>
@@ -156,20 +148,27 @@
 
     <script>
         const ctx = document.getElementById('myChart').getContext('2d');
+        var data = {!! json_encode($data) !!};
         const myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Oct' , 'Nov' , 'Dec'],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: 'orders by month {{ date('Y') }}',
+                    data: data,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        '#4B445320',
+                        'rgba(255, 159, 64, 0.2)',
+                        '#66BEA020',
+                        '#D65DB120',
+                        '#926C0020',
+                        '#005B4420',
+                        '#6100FF20',
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -177,7 +176,13 @@
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        '#4B4453',
+                        'rgba(255, 159, 64, 1)',
+                        '#66BEA0',
+                        '#D65DB1',
+                        '#926C00',
+                        '#005B44',
+                        '#6100FF',
                     ],
                     borderWidth: 1
                 }]
@@ -191,22 +196,31 @@
             }
         });
 
+        @php
 
+        @endphp
+        var salesByMonth = {!! json_encode($salesByMonth) !!};
         const orderByMonth = document.getElementById('orderByMonth').getContext('2d');
         const orderByMonthChart = new Chart(orderByMonth, {
             type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun' , 'Jul' , 'Aug' , 'Sep' , 'Oct' , 'Nov' , 'Dec'],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    label: 'sales by month {{ date('Y') }}',
+                    data: salesByMonth,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        '#4B445320',
+                        'rgba(255, 159, 64, 0.2)',
+                        '#66BEA020',
+                        '#D65DB120',
+                        '#926C0020',
+                        '#005B4420',
+                        '#6100FF20',
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -214,7 +228,13 @@
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        '#4B4453',
+                        'rgba(255, 159, 64, 1)',
+                        '#66BEA0',
+                        '#D65DB1',
+                        '#926C00',
+                        '#005B44',
+                        '#6100FF',
                     ],
                     borderWidth: 1
                 }]
